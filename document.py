1736cbdf148cb99120ab6fd9f352cb7db9b4ee8a
Indra Widyana <indrawidyana@gmail.com>
2019-09-04 18:02:46 +0700

    created python script for calculating similarity between two documents

--- document.py
+++ document.py
@@ -1,4 +1,20 @@
-def doc_similarity(doc1, doc2):
-    return similarity_value
+#%%
+from sklearn.feature_extraction.text import TfidfVectorizer
 
-print(doc_similarity(doc1, doc2))
+#%%
+data_location = './data/document/business/'
+doc1 = (data_location + '001.txt')
+doc2 = (data_location + '002.txt')
+
+#%%
+def doc_similarity(doc1, doc2):
+    documents = [open(doc1).read(), open(doc2).read()]
+    vectorized = TfidfVectorizer().fit_transform(documents)
+    similarity_vector = vectorized * vectorized.T
+    similarity_vector = similarity_vector.toarray()
+
+    # return similarity_value
+    return similarity_vector[0][1]
+
+#%%
+print(doc_similarity(doc1, doc2).toarray())
