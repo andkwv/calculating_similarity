0e2024ea9f3bab5815e670a8be2185f6fb1d7a14
andkwv <42143100+andkwv@users.noreply.github.com>
2019-09-04 23:48:44 +0700

    Update graph.py

--- graph.py
+++ graph.py
@@ -1,4 +1,42 @@
-def graph_similarity(graph_1, graph_2):
-    return similarity_value
+#%%
+from sklearn.feature_extraction.text import CountVectorizer
+import numpy
+from numpy import pad
 
-print(graph_similarity(graph_1, graph_2))
+#%%
+data_location = 'D:/KULIAH/PELAJARAN_SEMESTER_7/DATA MINING/Assignment_2/data/'
+doc1 = (data_location + 'chameleon.txt')
+doc2 = (data_location + 'crocodile.txt')
+
+
+def doc_similarity(doc1, doc2):
+    #
+    vocabulary = open(doc1).read().split(' ')
+    vocabulary.append(doc2)
+
+    vectorizer = CountVectorizer()
+    res = vectorizer.fit_transform(vocabulary).todense()
+    vocabulary = vectorizer.vocabulary_
+
+    #
+    doc1 = open(doc1).read().split(' ')
+    doc2 = open(doc2).read().split(' ')
+
+    #
+    vectorizer = CountVectorizer(vocabulary=vocabulary)
+    doc1 = vectorizer.fit_transform(doc1).todense()
+    doc2 = vectorizer.fit_transform(doc2).todense()
+
+    #
+    doc1 = doc1.sum(axis=0)
+    doc2 = doc2.sum(axis=0)
+
+    #%%
+    dot_product = float(numpy.inner(doc1, doc2))
+    doc1_length = numpy.linalg.norm(doc1)
+    doc2_length = numpy.linalg.norm(doc2)
+
+    return dot_product / (doc1_length * doc2_length)
+
+
+print(doc_similarity(doc1, doc2))
