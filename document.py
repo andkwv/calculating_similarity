e12448dab652319e639aa9667a0994f0eff1c590
Indra Widyana <indrawidyana@gmail.com>
2019-09-04 22:37:39 +0700

    changed similarity calculation of documents from if tdf to cosine similarity

--- document.py
+++ document.py
@@ -1,20 +1,40 @@
 #%%
-from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.feature_extraction.text import CountVectorizer
+import numpy
+from numpy import pad
 
 #%%
 data_location = './data/document/business/'
 doc1 = (data_location + '001.txt')
 doc2 = (data_location + '002.txt')
 
-#%%
 def doc_similarity(doc1, doc2):
-    documents = [open(doc1).read(), open(doc2).read()]
-    vectorized = TfidfVectorizer().fit_transform(documents)
-    similarity_vector = vectorized * vectorized.T
-    similarity_vector = similarity_vector.toarray()
+    #
+    vocabulary = open(doc1).read().split(' ')
+    vocabulary.append(doc2)
 
-    # return similarity_value
-    return similarity_vector[0][1]
+    vectorizer = CountVectorizer()
+    res = vectorizer.fit_transform(vocabulary).todense()
+    vocabulary = vectorizer.vocabulary_
 
-#%%
-print(doc_similarity(doc1, doc2).toarray())
+    #
+    doc1 = open(doc1).read().split(' ')
+    doc2 = open(doc2).read().split(' ')
+
+    #
+    vectorizer = CountVectorizer(vocabulary=vocabulary)
+    doc1 = vectorizer.fit_transform(doc1).todense()
+    doc2 = vectorizer.fit_transform(doc2).todense()
+
+    #
+    doc1 = doc1.sum(axis=0)
+    doc2 = doc2.sum(axis=0)
+
+    #%%
+    dot_product = float(numpy.inner(doc1, doc2))
+    doc1_length = numpy.linalg.norm(doc1)
+    doc2_length = numpy.linalg.norm(doc2)
+
+    return dot_product / (doc1_length * doc2_length)
+
+print(doc_similarity(doc1, doc2))
